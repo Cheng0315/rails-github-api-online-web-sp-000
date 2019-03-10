e5f5d3dc568688b150637a25b42bdf62109837b4
Cheng Vue <csalvue@gmail.com>
2019-03-10 21:41:57 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
 
     body = JSON.parse(response.body)
     session[:token] = body["access_token"]
+    binding.pry
     redirect_to root_path
   end
 
