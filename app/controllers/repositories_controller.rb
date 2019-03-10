f7375bf5b6a46fb6ff80ebd475df86256f698a42
Cheng Vue <csalvue@gmail.com>
2019-03-10 22:01:36 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,7 @@
 class RepositoriesController < ApplicationController
 
   def index
-    @resp = Faraday.get("https://github.com/login/oauth/authorize") do |req|
-      req.params['oauth_token'] = session[:token]
-    end
+
   end
 
 end
