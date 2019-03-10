3a5a17f23cffc15a76442b20323873fe9dbae645
Cheng Vue <csalvue@gmail.com>
2019-03-10 22:11:47 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,10 +1,12 @@
 class RepositoriesController < ApplicationController
 
   def index
-    @resp = Faraday.get("https://api.github.com/user/repos") do |req|
+    resp = Faraday.get("https://api.github.com/user/repos") do |req|
       req.headers['Authorization'] = "token #{session[:token]}"
       req.headers['Accept'] = "appication/json"
     end
+
+    @repo = JSON.parse(resp.body)
   end
 
 end
